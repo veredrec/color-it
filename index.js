a7abf1635cd7af422a9e303da70bf4c3fb4423e4
Vered Rekanati <veredrec@gmail.com>
2018-01-11 15:45:48 -0500

    fix basic app functionality

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ var currentElement;
 // });
 
 // COLOR ELEMENT
-colors = [
+colorsArray = [
   'yellow',
   'orange',
   'darkGreen',
@@ -108,40 +108,18 @@ circm.addEventListener('click', function() {
 circn.addEventListener('click', function() {
   currentElement = circn;
 });
-// Check if class with color already exists.
-// If it does - removes it to add the new class
-
-function hasColor(element, color) {
-  return (' ' + element.className + ' ').indexOf(' ' + color + ' ') > -1;
-}
 
 var colors = document.querySelectorAll('.color');
 colors.forEach(function(color) {
   color.addEventListener('click', function() {
-    console.log('adding the color ' + this.id + ' to ' + currentElement);
-    currentElement.classList.add(this.id);
-    for (var i = 0; i < colors.length; i++) {
-      if (hasColor(currentElement, colors[i])) {
-        console.log('removes ', colors[i]);
-        currentElement.classList.remove(colors[i]);
+    var classes = currentElement.classList;
+    for (var i = 0; i < colorsArray.length; i++) {
+      for (var j = 0; j < classes.length; j++) {
+        if (colorsArray[i] === classes[j]) {
+          currentElement.classList.remove(classes[j]);
+        }
       }
     }
+    currentElement.classList.add(this.id);
   });
 });
-
-// var yellow = document.getElementById('yellow');
-// var orange = document.getElementById('orange');
-// var darkGreen = document.getElementById('darkGreen');
-// var green = document.getElementById('green');
-// var turquoise = document.getElementById('turquoise');
-// var lightBlue = document.getElementById('lightBlue');
-// var peach = document.getElementById('peach');
-// var blue = document.getElementById('blue');
-// var purple = document.getElementById('purple');
-// var pink = document.getElementById('pink');
-// var red = document.getElementById('red');
-// var brown = document.getElementById('brown');
-// var black = document.getElementById('black');
-// var grey = document.getElementById('grey');
-// var white = document.getElementById('white');
-//
